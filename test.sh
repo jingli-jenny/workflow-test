37acaf69aa2148e22a148d511c3db336d28026d4
Jenny Li <jing.li@zilliz.com>
2021-12-27 21:53:17 +0800

    Add curl to test
    Signed-off-by: Jenny Li <jing.li@zilliz.com>

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ for i in {1..1000}
 
 do 
     echo "-------------$i-------------------------"
-    curl -L  https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-0.6.3.tar.gz > yaml-cpp-0.6.3.tar.gz
-    rm -rf yaml-cpp-0.6.3.tar.gz
+    # curl -L  https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-0.6.3.tar.gz > yaml-cpp-0.6.3.tar.gz
+    # rm -rf yaml-cpp-0.6.3.tar.gz
+    curl -L https://api.github.com/repos/jingli-jenny/workflow-test/actions/workflows
 done
